5e1bda2d6636ec82c8d697b054c6a06201e76173
Sam Wright <samwrighthwk@gmail.com>
2021-01-11 23:39:22 +0000

    trying to add multiple tables to dataframe

--- pollparser.py
+++ pollparser.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-import csv
 import pandas as pd
 import numpy as np
 
@@ -9,36 +8,43 @@ def getHTML(link):
     soup = BeautifulSoup(source,'lxml')
     return soup
 
-def getTables(soup,number)
+def getTables(soup,limit):
     tables = soup.find_all('table', class_='wikitable sortable')
-    tables = tables[:number]
-    for table in len(tables):
+    tables = tables[:limit]
+    for table in range(len(tables)):
         tables[table] = tables[table].tbody
     return tables
 
+def tableToDf(table,df,first):
+    rows = table.find_all('tr')
+    array = []
+    headers = rows[0].find_all('th')
 
-rows = table.find_all('tr')
+    for header in range(len(headers)):
+        headers[header] = headers[header].text.replace('\n', '')
+    rows.pop(0)
 
-#csvfile = open('pollresults.csv','w',newline='')
-#csvwriter = csv.writer(csvfile)
+    for row in rows:
+        data = row.find_all('td')
+        if len(data) == len(headers):
+            for datum in range(len(data)):
+                data[datum] = data[datum].text.replace('\n', '')
+            array.append(data)
 
-array = []
+    if first == True:
+        df = pd.DataFrame(array,columns=headers)
+        first = False
+    else:
+        df.append(array)
+    return df
 
-headers = rows[0].find_all('th')
-for header in range(len(headers)):
-    headers[header] = headers[header].text.replace('\n', '')
-rows.pop(0)
-#csvwriter.writerow(headers)
-
-for row in rows:
-    data = row.find_all('td')
-    print(len(data),len(headers))
-    if len(data) == len(headers):
-        for datum in range(len(data)):
-            data[datum] = data[datum].text.replace('\n', '')
-        #csvwriter.writerow(data)
-        array.append(data)
-
-df = pd.DataFrame(array,columns=headers)
-df.to_csv("pollresults.csv")
-#csvfile.close()
+source = "https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_United_Kingdom_general_election#2021"
+soup = getHTML(source)
+tables = getTables(soup,2)
+df = pd.DataFrame()
+first = True
+for table in tables:
+    df = tableToDf(table,df,first)
+    first = False
+print(df)
+#df.to_csv("pollresults.csv")
