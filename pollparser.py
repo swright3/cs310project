e6c11500f58b0ae43da6779e159495d3b5ea9d5c
Sam Wright <samwrighthwk@gmail.com>
2021-01-11 18:10:48 +0000

    2021 table to csv using csv package

--- pollparser.py
+++ pollparser.py
@@ -5,4 +5,25 @@ import csv
 source = requests.get('https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_United_Kingdom_general_election#2021').text
 soup = BeautifulSoup(source,'lxml')
 
-print(soup.prettify())
+table = soup.find('table', class_='wikitable sortable').tbody
+
+rows = table.find_all('tr')
+
+csvfile = open('pollresults.csv','w',newline='')
+csvwriter = csv.writer(csvfile)
+
+headers = rows[0].find_all('th')
+for header in range(len(headers)):
+    headers[header] = headers[header].text.replace('\n', '')
+rows.pop(0)
+csvwriter.writerow(headers)
+
+for row in rows:
+    data = row.find_all('td')
+    print(len(data),len(headers))
+    if len(data) == len(headers):
+        for datum in range(len(data)):
+            data[datum] = data[datum].text.replace('\n', '')
+        csvwriter.writerow(data)
+
+csvfile.close()
